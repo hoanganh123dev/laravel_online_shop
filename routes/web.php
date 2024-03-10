8926fab158ec6b5952ffce49aae2cf33aaa8df1c
Hoàng Anh <112646804+hoanganh123dev@users.noreply.github.com>
2024-03-10 10:42:59 +0700

    xong bai 9

--- routes/web.php
+++ routes/web.php
@@ -47,8 +47,12 @@ Route::group(['prefix'=> 'admin'], function () {
         Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.delete');
         
         //Sub Category Routes
+        Route::get('/sub-categories', [SubCategoryController::class, 'index'])->name('sub-categories.index');
         Route::get('/sub-categories/create', [SubCategoryController::class, 'create'])->name('sub-categories.create');
-        
+        Route::post('/sub-categories', [SubCategoryController::class, 'store'])->name('sub-categories.store');
+        Route::get('/sub-categories/{subCategory}/edit', [SubCategoryController::class, 'edit'])->name('sub-categories.edit');
+        Route::put('/sub-categories/{subCategory}', [SubCategoryController::class, 'update'])->name('sub-categories.update');
+        Route::delete('/sub-categories/{subCategory}', [SubCategoryController::class, 'destroy'])->name('sub-categories.delete');
         
         //temp-image.create
         Route::post('/upload-temp-image', [TempImagesController::class, 'create'])->name('temp-images.create');
